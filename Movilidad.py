00b482d25f76c08536c3aee1c6e2bf1bbf1c6fd6
Fredy Muñoz <47016508+FredyMunozS@users.noreply.github.com>
2019-03-05 20:44:15 -0500

    Update Movilidad.py

--- Movilidad.py
+++ Movilidad.py
@@ -369,7 +369,7 @@ def on_click(Num_Clicks, Recorrido):
 if __name__ == '__main__':
     app.run_server(
         debug=True,
-        #host='0.0.0.0',
+        host='0.0.0.0',
         port=5000
     )
     
