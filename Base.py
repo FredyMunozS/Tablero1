ddd0ff53990e11e07207acfd04aae849a568533e
Fredy Muñoz <47016508+FredyMunozS@users.noreply.github.com>
2019-04-14 23:55:29 -0500

    Add files via upload

---
+++ Base.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 14 23:51:19 2019
+
+@author: Admin
+"""
+
+from imdb import IMDb
+ia = IMDb()
+import joblib
+import pandas as pd
+
+
+
+Movies = joblib.load('Movies.pkl')
+Movies= sorted(Movies.Titulo.unique())
+
+Base = pd.DataFrame(columns=['Titulo', 'Poster'])
+j=1
+for i in Movies:
+    try:
+        print(j)
+        mov=ia.search_movie(i)[0]
+        movie = ia.get_movie(mov.movieID)
+        Base=Base.append( pd.DataFrame([[i,movie['cover url']]], columns=['Titulo', 'Poster']))
+    except:
+        continue
+    j=j+1
+    
+joblib.dump(Base,'Base.pkl')
