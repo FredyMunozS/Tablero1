efd14fd4f2f5ad3f549183be48b0bf59c8c56c4d
Fredy Muñoz <47016508+FredyMunozS@users.noreply.github.com>
2019-04-15 22:09:01 -0500

    Update Base.py

--- Base.py
+++ Base.py
@@ -15,16 +15,16 @@ import pandas as pd
 Movies = joblib.load('Movies.pkl')
 Movies= sorted(Movies.Titulo.unique())
 
-Base = pd.DataFrame(columns=['Titulo', 'Poster'])
+Base = pd.DataFrame(columns=['Titulo', 'Poster', 'Genero'])
 j=1
 for i in Movies:
     try:
         print(j)
         mov=ia.search_movie(i)[0]
         movie = ia.get_movie(mov.movieID)
-        Base=Base.append( pd.DataFrame([[i,movie['cover url']]], columns=['Titulo', 'Poster']))
+        Base=Base.append( pd.DataFrame([[i,movie['cover url'],movie['genres']]], columns=['Titulo', 'Poster', 'Genero']))
     except:
         continue
     j=j+1
     
-joblib.dump(Base,'Base.pkl')
+joblib.dump(Base,'Base2.pkl')
